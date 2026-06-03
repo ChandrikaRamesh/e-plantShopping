8e449b0ac05b56bc864fbe9db4b1a77b81da2528
ChandrikaRamesh <chandrikar28@gmail.com>
2026-06-03 04:13:03 -0400

    Changes to CartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,34 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.items.forEach(element => {
+        const quantity = element.quantity;
+        const cost = element.cost;
+        total = total + parseFloat(cost.substring(1)) * quantity;
+        return total;
+    });
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
+  
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item))
   };
 
   // Calculate total cost based on quantity for an item
